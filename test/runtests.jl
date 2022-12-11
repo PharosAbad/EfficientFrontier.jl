7db22c39083845ae7cec01e101c9a30e220e1b11
PharosAbad <pharos.abad@gmail.com>
2022-12-11 22:00:06 +0800

    runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,18 +8,12 @@ using Test
         1/100 1/40 1/25]
     E = [109 / 100; 23 / 20; 119 / 100]
 
-    #=  #v0.1.0
-    EfficientFrontier.noShortsale(E, V)
-    aCL = EfficientFrontier.ECL()
-    Z = EfficientFrontier.CornerP()
-    @test isapprox(Z[end,:]'*E, E[1])
-    =#
-    
-    #v0.2.0
+        
+    #v0.3.0
     P = Problem(E, V; equilibrate=false)
     aCL = EfficientFrontier.ECL(P)
     aEF = eFrontier(aCL, P)
 
-    @test length(aCL) == 3
+    @test length(aCL) == 4
     @test isapprox(aEF.Z[end,:]'*E, E[1])
 end
